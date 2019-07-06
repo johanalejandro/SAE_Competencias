6107730472b0a6a15b9eb44a6b99109ed3aa81f4
Ahtty <aahtty@espol.edu.ec>
2019-07-06 17:36:41 -0500

    Warnings removed

--- SAE/resources/views/welcome.blade.php
+++ SAE/resources/views/welcome.blade.php
@@ -3,6 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
         <title>Postulación</title>
 
