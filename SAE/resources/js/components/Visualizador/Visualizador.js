ba5be7373125cbc1852c625e3b9fdddf75934d66
Ahtty <aahtty@espo.edu.ec>
2019-08-31 01:05:05 -0500

    Visualizador corregido

--- SAE/resources/js/components/Visualizador/Visualizador.js
+++ SAE/resources/js/components/Visualizador/Visualizador.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import MUIDataTable from "mui-datatables";
 import Header from "../common/Header";
 import axios from "axios";
+import clone from 'lodash/clone';
+import findKey from 'lodash/findKey';
 
 export default class Visualizador extends Component {
 
@@ -21,8 +23,9 @@ export default class Visualizador extends Component {
         let habilitadosExperto = [];
         let habilitadosEvaluador = [];
         await axios.get("api/mostrarEvaluadoresHabilitado")
-        .then(async({data} ) => {
-            const postulantes = data.map((postulante)=>{
+        .then(({data} ) => {
+            console.log(data);
+            data.map(async(postulante,i)=>{
                 let post ={};
                 post['id_postulante'] = postulante.id_postulante;
                 post['nombres'] = postulante.nombres;
@@ -33,56 +36,25 @@ export default class Visualizador extends Component {
                 post['fechaHabilitacion'] = postulante.fechaHabilitacion;
                 post['disponibilidadViajar'] = postulante.disponibilidadViajar;
                 post['cedula'] = postulante.cedula;
-                return post;
-            })
-            const postulantesUnico = uniqBy(postulantes,'id_postulante');
-            const experiencias = data.map((postulante)=>{
-                let experiencia = {};
-                experiencia['id_postulante'] = postulante.id_postulante;
-                experiencia['nombreEmpresa'] = postulante.nombreEmpresa;
-                experiencia['cargoEjercido'] = postulante.cargoEjercido;
-                experiencia['descripcion'] = postulante.descripcion;
-                experiencia['id_sector_requerimiento'] = postulante.id_sector_requerimiento;
-                return experiencia;
-            })
-
-            let postulantesHabilitados = {};
-            for (let i = 0; i < postulantesUnico.length; i++) {
-                const postulante = postulantesUnico[i];
-                postulantesHabilitados['id_postulante'] = postulante.id_postulante;
-                        postulantesHabilitados['nombres'] = postulante.nombres;
-                        postulantesHabilitados['apellidos'] = postulante.apellidos;
-                        postulantesHabilitados['estado']= postulante.estado;
-                        postulantesHabilitados['email'] = postulante.email;
-                        postulantesHabilitados['tipoPostulacion'] = postulante.tipoPostulacion;
-                        postulantesHabilitados['fechaHabilitacion'] = postulante.fechaHabilitacion;
-                        postulantesHabilitados['disponibilidadViajar'] = postulante.disponibilidadViajar;
-                        postulantesHabilitados['cedula'] = postulante.cedula;
-                        postulantesHabilitados['experiencias']=[];
-                for (let index = 0; index < experiencias.length; index++) {
-                    const experiencia = experiencias[index];
-                    let exp = {};
-                    if (postulante.id_postulante===experiencia.id_postulante) {
-                        exp['nombreEmpresa'] = experiencia.nombreEmpresa;
-                        exp['cargoEjercido'] = experiencia.cargoEjercido;
-                        exp['descripcion'] = experiencia.descripcion;
-                        exp['requerimiento'] = await this.getRequerimiento(experiencia.id_sector_requerimiento);
-                        exp['tipoPostulacion'] = postulante.tipoPostulacion;
-                    }
-                    
-                    postulantesHabilitados['experiencias'].push(exp)
-                }
-                habilitadosEvaluador.push(postulantesHabilitados);
+                post['nombreEmpresa'] = postulante.nombreEmpresa;
+                post['cargoEjercido'] = postulante.cargoEjercido;
+                post['fechaInicio'] = postulante.fecha_inicio;
+                post['fechaFin'] = postulante.fecha_fin;
+                post['fechaNacimiento'] = postulante.fechaNacimiento;
+                post['estado'] = postulante.estado;
+                post['ciudad'] = postulante.ciudad;
+                post['provincia'] = postulante.provincia;
+                post['telefono'] = postulante.telefono;
+                post['sector'] = await this.getSector(parseInt(postulante.id_sector));
+                habilitadosEvaluador[i]=clone(post);
+            });
                 
-            }
         })
         .catch(console.error);
-        await this.setState({
-            habilitadosEvaluador: habilitadosEvaluador,
-        })
+        this.setState({habilitadosEvaluador:habilitadosEvaluador});
         await axios.get("api/mostrarExpertosHabilitado")
-        .then(async({data} ) => {
-            const postulantes = data.map((postulante)=>{
+        .then(({data} ) => {
+            data.map(async(postulante,i)=>{
                 let post ={};
                 post['id_postulante'] = postulante.id_postulante;
                 post['nombres'] = postulante.nombres;
@@ -93,53 +65,22 @@ export default class Visualizador extends Component {
                 post['fechaHabilitacion'] = postulante.fechaHabilitacion;
                 post['disponibilidadViajar'] = postulante.disponibilidadViajar;
                 post['cedula'] = postulante.cedula;
-                return post;
-            })
-            const postulantesUnico = uniqBy(postulantes,'id_postulante');
-            const experiencias = data.map((postulante)=>{
-                let experiencia = {};
-                experiencia['id_postulante'] = postulante.id_postulante;
-                experiencia['nombreEmpresa'] = postulante.nombreEmpresa;
-                experiencia['cargoEjercido'] = postulante.cargoEjercido;
-                experiencia['descripcion'] = postulante.descripcion;
-                experiencia['id_alcance'] = postulante.id_alcance;
-                return experiencia;
-            })
-
-            let postulantesHabilitados = {};
-            for (let i = 0; i < postulantesUnico.length; i++) {
-                const postulante = postulantesUnico[i];
-                postulantesHabilitados['id_postulante'] = postulante.id_postulante;
-                        postulantesHabilitados['nombres'] = postulante.nombres;
-                        postulantesHabilitados['apellidos'] = postulante.apellidos;
-                        postulantesHabilitados['estado']= postulante.estado;
-                        postulantesHabilitados['email'] = postulante.email;
-                        postulantesHabilitados['tipoPostulacion'] = postulante.tipoPostulacion;
-                        postulantesHabilitados['fechaHabilitacion'] = postulante.fechaHabilitacion;
-                        postulantesHabilitados['disponibilidadViajar'] = postulante.disponibilidadViajar;
-                        postulantesHabilitados['cedula'] = postulante.cedula;
-                        postulantesHabilitados['experiencias']=[];
-                for (let index = 0; index < experiencias.length; index++) {
-                    const experiencia = experiencias[index];
-                    let exp = {};
-                    if (postulante.id_postulante===experiencia.id_postulante) {
-                        exp['nombreEmpresa'] = experiencia.nombreEmpresa;
-                        exp['cargoEjercido'] = experiencia.cargoEjercido;
-                        exp['descripcion'] = experiencia.descripcion;
-                        exp['alcance'] = await this.getAlcance(experiencia.id_alcance);
-                        exp['tipoPostulacion'] = postulante.tipoPostulacion;
-                    }
-                    
-                    postulantesHabilitados['experiencias'].push(exp)
-                }
-                habilitadosExperto.push(postulantesHabilitados);
+                post['nombreEmpresa'] = postulante.nombreEmpresa;
+                post['cargoEjercido'] = postulante.cargoEjercido;
+                post['fechaInicio'] = postulante.fecha_inicio;
+                post['fechaFin'] = postulante.fecha_fin;
+                post['estado'] = postulante.estado;
+                post['fechaNacimiento'] = postulante.fechaNacimiento;
+                post['ciudad'] = postulante.ciudad;
+                post['provincia'] = postulante.provincia;
+                post['telefono'] = postulante.telefono;
+                post['alcance'] = await this.getAlcance(parseInt(postulante.id_alcance));
+                habilitadosExperto[i]=clone(post);
+            });
                 
-            }
         })
         .catch(console.error);
-        await this.setState({
-            habilitadosExperto: habilitadosExperto,
-        })
+        this.setState({ habilitadosExperto: habilitadosExperto });
 
 }
 
@@ -170,6 +111,36 @@ export default class Visualizador extends Component {
             })
         }
     }
+
+    getSector  =async (id) => {
+        let sector = "";
+        await axios.get('/api/sector/')
+        .then(({data} )=> {
+            const sectoresResponse = clone(data);
+                const key = findKey(sectoresResponse,(sector)=>{
+                    return sector.id_sector===id;
+                })
+                sector = sectoresResponse[key].tipoSector;
+        }).catch(error => {
+            console.log("===ERROR: ",error);
+        });
+        return sector;
+    }
+    
+        getAlcance = async (id) => {
+            let alcance = ""
+            await axios.get('/api/alcance')
+            .then(({data}) => {
+                const alcancesResponse = clone(data);
+                const key = findKey(alcancesResponse,(alcance)=>{
+                    return alcance.id_alcance===id;
+                })
+                alcance = alcancesResponse[key].nombreAlcance;
+            }).catch(error => {
+                console.log("===ERROR: ",error);
+            });
+            return alcance;
+        }
    
     render() {
         console.log("POSTULANTE RESPONSE",this.state.habilitadosEvaluador);
@@ -212,14 +183,6 @@ export default class Visualizador extends Component {
              }
             },
             {
-             name: "tipoPostulacion",
-             label: "Tipo de Postulación",
-             options: {
-              filter: true,
-              sort: true,
-             }
-            },
-            {
                 name: "cedula",
                 label: "Cédula",
                 options: {
@@ -227,14 +190,39 @@ export default class Visualizador extends Component {
                  sort: true,
                 },
                },
-            {
-             name: "email",
-             label: "Correo",
-             options: {
-              filter: true,
-              sort: false,
-             },
-            },
+               {
+                name: "sector",
+                label: "Sector Habilitado",
+                options: {
+                 filter: true,
+                 sort: true,
+                 display: this.state.tipo === "habilitadosEv"?true:false,
+                 customBodyRender: (value) => {
+                     if(this.state.tipo === "habilitadosEv"){
+                        return (
+                            <span className="text-normal text-info">{value}</span>
+                        );
+                     }
+                  },
+                },
+               },
+               {
+                name: "alcance",
+                label: "Alcance Habilitado",
+                options: {
+                 filter: true,
+                 sort: true,
+                 display: this.state.tipo === "habilitadosExp"?true:false,
+                 customBodyRender: (value) => {
+                    if(this.state.tipo === "habilitadosExp"){
+                       return (
+                           <span className="text-normal text-info">{value}</span>
+                       );
+                    }
+                 },
+                },
+               },
+               
             {
                 name: "estado",
                 label: "Estado",
@@ -242,33 +230,98 @@ export default class Visualizador extends Component {
                  filter: true,
                  sort: true,
                  customBodyRender: (value) => {
-                    if(toUpper(value)==="POR ASIGNAR"){
                         return (
-                            <span className="text-warning-dark">
+                            <span className="text-normal text-success">
                               {value}
                             </span>
                         );
-                    }
-                    if(toUpper(value)==="POR HABILITAR"){
-                        return (
-                            <span className="text-success">
-                              {value}
-                            </span>
-                        );
-                    }
-                    if(toUpper(value)==="HABILITADO"){
-                        return (
-                            <span className="text-normal">
-                              {value}
-                            </span>
-                        );
-                    }
                   },
                 },
                },
                {
+                name: "nombreEmpresa",
+                label: "Empresa de experiencia",
+                options: {
+                 filter: true,
+                 sort: true,
+                },
+               },
+               {
+                name: "cargoEjercido",
+                label: "Cargo de Experiencia",
+                options: {
+                 filter: false,
+                 sort: true,
+                },
+               },
+               {
+                name: "fechaInicio",
+                label: "Inicio de Experiencia",
+                options: {
+                 filter: false,
+                 sort: true,
+                },
+               },
+               {
+                name: "fechaFin",
+                label: "Fin de Experiencia",
+                options: {
+                 filter: false,
+                 sort: true,
+                },
+               },
+            {
+             name: "email",
+             label: "Correo",
+             options: {
+              filter: false,
+              sort: true,
+             },
+            },
+            
+            {
+                name: "tipoPostulacion",
+                label: "Tipo de Postulación",
+                options: {
+                 filter: true,
+                 sort: true,
+                }
+               },
+               {
                 name: "fechaHabilitacion",
                 label: "Fecha de Habilitación",
+                options: {
+                 filter: true,
+                 sort: true,
+                },
+               },
+               {
+                name: "fechaNacimiento",
+                label: "Fecha de Nacimiento",
+                options: {
+                 filter: true,
+                 sort: true,
+                },
+               },
+               {
+                name: "telefono",
+                label: "Teléfono",
+                options: {
+                 filter: true,
+                 sort: true,
+                },
+               },
+               {
+                name: "ciudad",
+                label: "Ciudad",
+                options: {
+                 filter: true,
+                 sort: true,
+                },
+               },
+               {
+                name: "provincia",
+                label: "Provincia",
                 options: {
                  filter: true,
                  sort: true,
@@ -286,63 +339,6 @@ export default class Visualizador extends Component {
                         }else{
                             return <span>No</span>
                         }
-                  },
-                },
-               },
-               {
-                name:"estado",
-                label: "Acciones",
-                options: {
-                 filter: true,
-                 sort: false,
-                 display: this.state.tipo==="habilitadosExp" || this.state.tipo === "habilitadosEv"?false:true,
-                 customHeadRender: (value) => {
-                    display: false;
-                    filter: false;
-                    sort: false;
-                    searcheable: false;
-                 },
-                 customBodyRender: (value, {rowData}, updateValue) => {
-                    if(toUpper(value)==="POR ASIGNAR"){
-                        return (
-                            <button className="btn-secondary" onClick={async() =>{
-                                await this.getUsuario(rowData);
-                                await this.selectModalAsignar ({
-                                    type: "Asignar",
-                                    data: rowData,
-                                })
-                            } }>Asignar</button>
-                        );
-                    }
-                    if(toUpper(value)==="POR HABILITAR"){
-                        return (
-                            <button className="btn-secondary" onClick={() => this.selectModal({
-                                type:'Habilitar',
-                                data: rowData,
-                                updateValue: updateValue,
-                            })
-                            }>Habilitar</button>
-                        );
-                    }
-                  },
-                },
-               },
-               {
-                name:"experiencias",
-                label: "Experiencias",
-                options: {
-                 filter: false,
-                 sort: false,
-                 display: this.state.tipo==="habilitadosExp" || this.state.tipo === "habilitadosEv"?true:false,
-                 customBodyRender: (value, {rowData}, updateValue) => {
-                        return (
-                            <button className="btn-secondary" onClick={() => this.selectModal({
-                                type: "Experiencias",
-                                data: value,
-                                updateValue: updateValue,
-                            })
-                            }>Experiencias</button>
-                        );
                   },
                 },
                },
